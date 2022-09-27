fd706e3942e8e9ed832abc9a874bc4e0cf776bd6
ja2w0nii <jaewoniik94@gmail.com>
2022-09-27 09:44:41 +0900

    22/09/27 쪽지시험

--- introduce/admin.py
+++ introduce/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AccessLogModel
 
+# Register your models here.
+admin.site.register(AccessLogModel)
 # Register your models here.
--- introduce/models.py
+++ introduce/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class AccessLogModel(models.Model):
+    class Meta:
+        db_table = "AccessLog"
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=256)
